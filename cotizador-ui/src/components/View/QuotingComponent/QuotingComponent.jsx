7ee94febbe14b82732bde204992a7e30935858a5
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2019-01-16 22:08:57 -0600

    Quoting component init

--- cotizador-ui/src/components/View/QuotingComponent/QuotingComponent.jsx
+++ cotizador-ui/src/components/View/QuotingComponent/QuotingComponent.jsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import './QuotingComponent.css';
 
 class QuotingComponent extends Component {
-    state = {  }
-    render() { 
-        return ( 
+    cardHeader = 'cotizador de seguros';
+    state = {}
+    render() {
+        return (
             <div className="card">
-                <div className="card_header"></div>
+                <div className="card_header">{this.cardHeader}</div>
                 <div className="card_form"></div>
-                <div className="card-separator"></div>
                 <div className="card_resume"></div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default QuotingComponent;
 
